7e693e2e0d229f2a2a4dfcf4621a088ebb70c800
silenceper <silenceper@gmail.com>
2015-05-21 15:56:28 +0800

    add func demo

---
+++ func/demo1.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+var (
+    i int = 10
+)
+var j int = 20
+
+func add(a int,b int)int{
+    return a+b
+}
+
+func main(){
+    sum :=add(i,j)
+    fmt.Printf("%d+%d=%d\n",i,j,sum)
+}
