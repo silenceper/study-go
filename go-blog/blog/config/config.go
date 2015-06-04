3d40e327abaaa28d077187111d3c853bfbcc1b66
silenceper <silenceper@gmail.com>
2015-06-04 11:01:39 +0800

    update

--- go-blog/blog/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-
-var(
-    CFG_NAME="my blog"
-    CFG_DSN="root:123@tcp(localhost:3306)/blog?charset=utf8"
-    CFG_TPL_DIR="/root/workspaces/go/go-blog/blog/views/"
-    CFG_STATIC_DIR="/root/workspaces/go/go-blog/staticDir/"
-)
